2ad1868ed599617d8b1329d5dc87511368f0bdd6
Jose Belmin <113086888+JoseBelmin@users.noreply.github.com>
2025-06-01 17:56:29 +0400

    1st design implementation

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,13 @@
+// frontend/src/app/components/header/header.component.ts
+
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-header', 
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  // Add any properties or methods needed for your header
 }
